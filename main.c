48505370459a507e28274b5d3162bdee82a4dacc
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-09-16 20:13:28 +0200

    The basic binary operators work

--- main.c
+++ main.c
@@ -1,27 +1,72 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<assert.h>
 
 #define DELIM " "
 #define MAX_TOKENS 50
+#define ASSERT_M(exp, msg) assert(((void)msg, exp))
+
+typedef struct Stack {
+  float stack[MAX_TOKENS];
+  size_t top;
+} Stack;
 
 void print_token_list(char*[]);
-void tokenize_string(char[], char*[]);
+int tokenize_string(char[], char*[]);
+void print_stack(Stack);
 
 int main(void) {
-  char *token_list[MAX_TOKENS] = {0};
-
-  char calc_string[] = "1 2 + 3 *";
+  char calc_string[] = "1 2 + 4 7 / 22 - *";
   printf("Input string: \"%s\"\n", calc_string);
 
-  tokenize_string(calc_string, token_list);
+  char *token_list[MAX_TOKENS] = {0};
+  int token_count = tokenize_string(calc_string, token_list);
+  // printf("Token list: ");
+  // print_token_list(token_list);
 
-  print_token_list(token_list);
+  Stack s = {0};
+  for (int i=0; i<token_count; i++) {
+    if (strcmp(token_list[i], "+") == 0) {
+      float b = s.stack[--s.top];
+      float a = s.stack[--s.top];
+      s.stack[s.top++] = a + b;
+    }
+    else if (strcmp(token_list[i], "-") == 0) {
+      float b = s.stack[--s.top];
+      float a = s.stack[--s.top];
+      s.stack[s.top++] = a - b;
+    }
+    else if (strcmp(token_list[i], "*") == 0) {
+      float b = s.stack[--s.top];
+      float a = s.stack[--s.top];
+      s.stack[s.top++] = a * b;
+    }
+    else if (strcmp(token_list[i], "/") == 0) {
+      float b = s.stack[--s.top];
+      float a = s.stack[--s.top];
+      s.stack[s.top] = a / b;
+      s.top++;
+    } else {
+      s.stack[s.top++] = strtof(token_list[i], NULL);
+    }
+  }
+
+  ASSERT_M(s.top == 1, "Stack not properly consumed");
+  printf("Answer ==> %f\n", s.stack[--s.top]);
 
   return 0;
 }
 
-void tokenize_string(char input_string[], char *token_list[]) {
+void print_stack(Stack stack) {
+  printf("Stack: ");
+  for (int i=0; i<stack.top; i++) {
+    printf("%f ", stack.stack[i]);
+  }
+  printf("\n");
+}
+
+int tokenize_string(char input_string[], char *token_list[]) {
   char *tok = strtok(input_string, DELIM);
   if (tok == NULL) {
     printf("ERR: Input string is empty.");
@@ -30,11 +75,13 @@ void tokenize_string(char input_string[], char *token_list[]) {
 
   int token_counter = 0;
   do {
-    printf("Token %d: \"%s\"\n", token_counter, tok);
+    // printf("Token %d: \"%s\"\n", token_counter, tok);
     token_list[token_counter++] = tok;
     tok = strtok(NULL, DELIM);
   } 
   while (tok != NULL);
+
+  return token_counter;
 }
 
 void print_token_list(char* str_array[]) {
